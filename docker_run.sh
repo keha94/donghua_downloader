812c92ea1d6589a2c30949bfdf6d1e1bc9576960
Kevin Ha <kha@networkten.com.au>
2026-05-09 18:18:13 +1000

    add docker run

---
+++ docker_run.sh
@@ -0,0 +1,14 @@
+docker build -t donghua-downloader .
+
+docker stop donghua-downloader
+docker container rm donghua-downloader
+
+docker run -d -p 8765:8765 \
+    --name donghua-downloader \
+    --env-file .env \
+    -e APP_DATA_PATH=/app/app_data.json \
+    -v "$(pwd)/config.json:/app/config.json" \
+    -v "$(pwd)/app_data.json:/app/app_data.json" \
+    -v /volume1/Movies/Donghua:/downloads \
+    -v /volume1/Movies/Others:/volume1/Movies/Others \
+    donghua-downloader
